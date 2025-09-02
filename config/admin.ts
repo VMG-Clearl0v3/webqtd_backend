9efb4bdbe8e6a469b3ac04054ff0ea846747ac72
Ngân Nè <ph.vtru0ng.1610@gmail.com>
2025-09-02 14:42:43 +0700

    deploy render

--- config/admin.ts
+++ config/admin.ts
@@ -1,20 +1,28 @@
+// export default ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   secrets: {
+//     encryptionKey: env('ENCRYPTION_KEY'),
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+// });
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'defaultAdminSecret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    salt: env('API_TOKEN_SALT', 'defaultApiTokenSalt'),
   },
 });
